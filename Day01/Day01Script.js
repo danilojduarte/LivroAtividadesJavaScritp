cbde29bb9fceb234a878f05919bff1f256c77813
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-19 01:00:44 -0300

    Dia 01 concluido (Console & Alert)

--- Day01/Day01Script.js
+++ Day01/Day01Script.js
@@ -8,3 +8,14 @@
 
 
 // *var idade = prompt("Informe sua idade", "Digite aqui");
+// console.log("Seu nome é");
+// console.log(nome);
+
+// console.log("Seu nome é " + nome);
+// Return => Seu nome é Danilo Duarte
+
+// console.log("Seu nome é " + nome + "E sua idade é " + idade);
+// return => Seu nome é Danilo DuarteE sua idade é 38 
+
+// alert("Seu nome é " + nome + "E sua idade é " + idade + "anos");
+// return => Foi apresentado o alert coma mensagem "Seu nome é Danilo DuarteE sua idade é 38 anos"
