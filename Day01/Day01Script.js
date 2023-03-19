7b5f55cb91502aedb839ff54a88f4f4fa621eec1
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-19 00:55:24 -0300

    PrimeirosComandos JavaScript

---
+++ Day01/Day01Script.js
@@ -0,0 +1,10 @@
+// Iniciando estudos com JavaScript, através do livro "Começando com JavaScript"
+
+// Dia 01 (19 de março 2023)
+
+// Primeiros Comando utilizando o console.log
+
+// var nome = prompt("Informe seu nome", "Digiteaqui");
+
+
+// *var idade = prompt("Informe sua idade", "Digite aqui");
