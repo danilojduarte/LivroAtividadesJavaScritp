1c95b82eb49330597a4e6d25db9d3e1d49665f2e
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-20 15:10:11 -0300

    Aula concluida

---
+++ Day02/Day02Operadores.js
@@ -0,0 +1,21 @@
+// Operadores Relacionais & Aritmétricos;
+
+
+// 4%3
+// 1
+// 4**2
+// 16
+// clear;
+// ƒ clear() { [native code] }
+// clear();
+// VM310:1 console.clear() was prevented due to 'Preserve log'
+// undefined
+// clear;
+// ƒ clear() { [native code] }
+// clear();
+// VM363:1 console.clear() was prevented due to 'Preserve log'
+// undefined
+// 5+10+7+1/4
+// 22.25
+// (5+10+7+1)/4
+// 5.75
